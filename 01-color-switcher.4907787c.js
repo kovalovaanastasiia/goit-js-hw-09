d7e29812cff7f778cf2aa039265c1cdfed3e0ff5
kovalovaanastasiia <115465429+kovalovaanastasiia@users.noreply.github.com>
2023-03-18 22:28:53 +0000

    Deploying to gh-pages from @ kovalovaanastasiia/goit-js-hw-09@a6d8d0dfddbaa1330753b1c98a61decd11e5c5c3 🚀

---
+++ 01-color-switcher.4907787c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=null;function r(){e.setAttribute("disabled","true")}t.addEventListener("click",(function r(){t.setAttribute("disabled","true"),e.removeAttribute("disabled"),document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),o=setTimeout(r,1e3)})),e.addEventListener("click",(function(){r(),clearInterval(o),t.removeAttribute("disabled")})),r()}();
+//# sourceMappingURL=01-color-switcher.4907787c.js.map
